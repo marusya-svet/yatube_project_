95d4574a4f6b251867fa7562e43778c5ce00de67
Мария Светличная <mashasvetvik@gmail.com>
2023-05-07 13:06:00 +0300

    проверка подписки

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -135,7 +135,7 @@ def follow_index(request):
 def profile_follow(request, username):
     author = get_object_or_404(User, username=username)
     if author != request.user:
-        Follow.objects.create(
+        Follow.objects.get_or_create(
             user=request.user,
             author=author,
         )
